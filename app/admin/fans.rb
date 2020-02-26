85ad979d0ea293efaafbe9d086474d46fefb8a36
Raghu Betina <raghu@firstdraft.com>
2020-02-26 22:38:04 +0000

    Whitelist columns in Fan for ActiveAdmin

--- app/admin/fans.rb
+++ app/admin/fans.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Fan do
+
+ permit_params :fan_id, :photo_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
